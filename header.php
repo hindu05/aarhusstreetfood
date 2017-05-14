58c32834b8b27744052264c59aaa1b093b3b1756
hindu05 <hindu05@hotmail.com>
2017-05-14 22:14:21 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+<div id="navigation" class="col-md-offset-1 col-md-11 col-lg-offset-2 col-lg-10">
+
+	<nav><img id="logo" class="img-responsive hidden-sm hidden-md hidden-lg hidden-xl" src="img/logo.png"></li>
+		<ul class="hidden-xs">
+			<a href="index.php"><li><img id="logo" class="img-responsive" src="img/logo.png"></li></a>
+			<li><a href="" title="">Køkkener</a></li>		
+			<li><a href="om.php" title="">Om Os</a></li>
+			<li><a href="" title="">Selskaber</a></li>
+			<li><a href="" title="">Kontakt</a></li>		
+		</ul>
+	</nav>
+</div>
+<div class="clearfix"></div>
---
+++ om.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Oplev ægte street food i Aarhus med mad og drikke fra mere end 30 gadekøkkener og flere barer. Besøg Aarhus Street Food på rutebilstationen i hjertet af Aarhus.">
+<meta name="keywords" content="Aarhus, street food, mad, drikke, spisested">
+<meta name="author" content="Andreas Nielsen, Emma Schumacher, Mathilde Gladov, Michael Rønne">
+<title>Restaurant og café | Aarhus Street Food</title>
+<link rel="shortcut icon" type="image/ico" href="img/favicon.ico">
+<!-- Bootstrap -->
+<link href="css/reset.css" rel="stylesheet">
+<link href="css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/slick.css"/>
+
+<link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
+<link href="css/style.css" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Roboto+Condensed|Londrina+Outline|Londrina+Solid" rel="stylesheet">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+</head>
+<body>
+<?php include 'header.php';?>
+
+
+<div class="col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10">
+		<img class="img-responsive" src="img/banner-07.jpg">
+</div>
+<div class="clearfix clearheight"></div>
+<section id="omos" class="col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">
+<article class="col-md-6 col-lg-6">
+	<h2>Om Os</h2>
+	<p>Dét skal vi også have i Aarhus.</p>
+	<p>Det var tanken, da Thomas og Britt besøgte Borough Market i London!</p>
+	<p>Og så gik den vilde jagt efter en indendørs lokation - for det bliver altså kedeligt at spise lækker mad, hvis man skal stå i regnvejr, og derfor var det nødvendigt med tag over hovedet.
+</p>
+	<p>Stille og roligt fik de kontakt til en masse mennesker, der havde spændende ideer til veltillavet, ærlig mad, men stedet manglede stadig…</p>
+	<p>Indtil en dag hvor den gamle garage ved Aarhus Rutebilsstation åbenbarede sig (indsæt selv lyden af englesang). Garagens rå vægge, det store ovenlysvindue og en fremragende placering.  
+</p>
+	<p>Aarhus-Iværksætteren Kasper Kragelund kom med på projektet, og så begyndte arbejdet med at gøre Garagen klar til, at duften af mad for alvor kunne brede sig.</p>
+	<p>Der blev stiftet en stadeforening, så madboderne i fællesskab kan tage beslutninger om udsmykning, arrangementer osv. Køkkenerne kom op, der blev sat et musikanlæg op og duften af hjemmelavet, autentisk mad begyndte at brede sig.</p>
+	<p>Dørene blev slået op den 19. august 2016. Det er uhøjtideligt, ærligt, råt og hamrende velsmagende - Det er Aarhus Street Food.</p>
+	<p>Og det er også i regnvejr!</p>
+</article>
+<img class="col-md-6 col-lg-6" src="img/omos.jpg">
+	
+</section>
+<div class="clearfix clearheight"></div>
+</div-->
+<?php include 'footer.php';?>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-3.1.1.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/slick.min.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
