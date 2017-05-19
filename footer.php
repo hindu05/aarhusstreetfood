275595023f43bc033c84b5cbecfe8ba7da24ae26
hindu05 <hindu05@hotmail.com>
2017-05-19 15:13:31 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,19 @@
 <div class="clearfix"></div>
-<div id="footwrap" class="col-md-12 col-lg-12">
+		<div class="some xs-some">
+			<a href="https://www.facebook.com/aarhusstreetfood/" title="Facebook"><img alt="facebook logo" src="img/icons/facebooklogo.png"></a>
+			<a href="https://www.instagram.com/aarhusstreetfood/" title="Instagram"><img alt="instagram logo" src="img/icons/instagram.png"></a>
+			<a href="https://www.tripadvisor.dk/Restaurant_Review-g189530-d10767038-Reviews-Aarhus_Street_Food-Aarhus_East_Jutland_Jutland.html" title="Tripadvisor">
+			<img alt="tripadvisor" src="img/icons/tripadvisor.png"></a>
+		</div>
+<div id="footwrap" class="">
 	<footer class="col-md-offset-1 col-md-10 col-lg-offset-1 col-lg-10">
+
+		
 		<div class="openinghours col-md-4 col-lg-4">
 			<h2>Åbningstider</h2>
 			<p>Man - tors 11.30 - 21.00</p>
-			<P>Fredag 11.30 - 22.00 (bar til 24.00)
-			<P>Lørdag 11.30 - 22.00 (bar til 24.00)
+			<P>Fredag 11.30 - 22.00
+			<P>Lørdag 11.30 - 22.00
 			<P>Søndag 11.30 - 21.00</p>
 			<h2>Kaffebaren</h2>
 			<p>Søn - tors 09.00 - 21.00</p>
@@ -16,18 +24,26 @@
 
 		</div>
 		<div class="map col-md-4 col-lg-4">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d881.926995751084!2d10.209286977324263!3d56.15163273524591!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x567589dbccff414a!2sAarhus+Street+Food!5e0!3m2!1sda!2sdk!4v1494258829181" width="100%" height="350" frameborder="1" allowfullscreen></iframe>
-			<p>Ny Banegårdsgade 46
-			- 8000 Aarhus C
-			- DK</p>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d881.926995751084!2d10.209286977324263!3d56.15163273524591!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x567589dbccff414a!2sAarhus+Street+Food!5e0!3m2!1sda!2sdk!4v1494258829181" width="100%" height="350" allowfullscreen></iframe>
+			<p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+			<span itemprop="streetAddress">Ny Banegårdsgade 46</span>
+						- 
+			<span itemprop="postalCode">8000</span> 
+			<span itemprop="addressLocality">Aarhus C</span>
+						- 
+			<span itemprop="addressCountry">DK</span></p>
 		</div>
-		<div class="some col-md-4 col-lg-4">
-			<h2>Sociale Medier</h2>
-				<a href="https://www.facebook.com/aarhusstreetfood/" title="Facebook"><img src="img/icons/facebooklogo.png"></a>
-				<a href="https://www.instagram.com/aarhusstreetfood/" title="Instagram"><img src="img/icons/instagram.png"></a>
-			
+		<div class="some col-md-4 col-lg-4 hidden-xs hidden-sm">
+				<a href="https://www.facebook.com/aarhusstreetfood/" title="Facebook"><img alt="facebook logo" src="img/icons/facebooklogo.png"></a>
+				<a href="https://www.instagram.com/aarhusstreetfood/" title="Instagram"><img alt="instagram logo" src="img/icons/instagram.png"></a>
+				<a href="https://www.tripadvisor.dk/Restaurant_Review-g189530-d10767038-Reviews-Aarhus_Street_Food-Aarhus_East_Jutland_Jutland.html" title="Tripadvisor">
+				<img alt="tripadvisor logo" src="img/icons/tripadvisor.png"></a>
 		</div>
 
 	</footer>
-	<img id="skyline" src="img/skyline.png">
+	<img id="skyline" alt="skyline image" src="img/skylineaarhus03.png">
+	<div id="copyright">
+				
+	<span itemprop="name">Aarhus Street Food</span> &copy; <?php echo date("Y"); ?></span>
+	</div>
 </div>
