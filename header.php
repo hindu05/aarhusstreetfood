a4d49aca2ebaa6764c8137e7d2a92a49056c0050
hindu05 <hindu05@hotmail.com>
2017-05-19 15:13:59 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,13 +1,49 @@
-<div id="navigation" class="col-md-offset-1 col-md-11 col-lg-offset-2 col-lg-10">
 
-	<nav><img id="logo" class="img-responsive hidden-sm hidden-md hidden-lg hidden-xl" src="img/logo.png"></li>
-		<ul class="hidden-xs">
-			<a href="index.php"><li><img id="logo" class="img-responsive" src="img/logo.png"></li></a>
-			<li><a href="" title="">Køkkener</a></li>		
-			<li><a href="om.php" title="">Om Os</a></li>
-			<li><a href="" title="">Selskaber</a></li>
-			<li><a href="" title="">Kontakt</a></li>		
+<nav id="phonemenu" class="visible-xs">
+	<a href="index.php"><img alt="logo" itemprop="image" src="img/logo.png"></a>
+	<section id="burger">
+		<div class="burgerline"></div>		
+		<div class="burgerline"></div>
+		<div class="burgerline"></div>		
+	</section>
+	<div id="popupnav">
+		<ul>
+			<li><a id="kitchenscroll" href="index.php#køkkener" title="">Køkkener</a></li>		
+			<li><a href="om_os.php">Om Os</a></li>
+				<div class="submenu">
+					<a href="praktisk_info.php">Praktisk Info</a>
+				</div>
+			<li><a href="events.php" title="">Events</a></li>
+			<li><a href="kontakt.php" title="">Kontakt</a>
+				<div class="submenu">
+					<a href="presse.php">Presse</a>
+					<a href="selskaber.php">Selskaber</a>
+					<a href="ny_stand.php">Ny stand</a>
+				</div>
+			</li>		
+		</ul>
+	</div>
+</nav>
+<span itemscope itemtype="http://schema.org/Restaurant"><div id="navigation" class="hidden-xs col-md-offset-1 col-sm-offset-1 col-md-11 col-lg-offset-2 col-lg-10">
+	<nav>
+		<ul>
+			<a href="index.php"><li><img alt="logo" itemprop="image" id="logo" class="img-responsive" src="img/logo.png"></li></a>
+			<li class="navbtn"><a href="index.php#køkkener" id="kitchenscroll" title="">Køkkener</a></li>		
+			<li class="navbtn dropbtn"><a href="om_os.php" title="">Om Os</a>
+				<div class="dropdown">
+					<a href="praktisk_info.php">Praktisk Info</a>
+				</div>
+			</li>
+			<li class="navbtn"><a href="events.php" title="">Events</a></li>
+			<li class="navbtn dropbtn"><a href="kontakt.php" title="">Kontakt</a>
+				<div class="dropdown">
+					<a href="presse.php">Presse</a>
+					<a href="selskaber.php">Selskaber</a>
+					<a href="ny_stand.php">Ny stand</a>
+				</div>
+			</li>		
 		</ul>
 	</nav>
 </div>
 <div class="clearfix"></div>
+<div class="visible-xs xs-menu-fill"></div>
